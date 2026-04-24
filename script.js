69f494f30e3c2e4010dbf0de4992b1b410f29e77
Deepika6925 <vadladeepika6925@gmail.com>
2026-04-24 10:10:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let error = document.getElementById("error");
+
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (name === "" || email === "") {
+        error.textContent = "All fields are required!";
+    } 
+    else if (!email.match(emailPattern)) {
+        error.textContent = "Invalid email format!";
+    } 
+    else {
+        error.textContent = "Form submitted successfully!";
+    }
+});
